e219603561fae9b93125bc051b127bd3c7d80b4b
yuanyuli <tian.zhang@yoai.com>
2016-09-20 18:27:57 +0800

    QQAPI调整

--- ios/qq/QQModule.m
+++ ios/qq/QQModule.m
@@ -25,59 +25,19 @@ static RCTResponseSenderBlock authCallback;
 
 RCT_EXPORT_MODULE(QQ);
 //注册App
-RCT_EXPORT_METHOD(registerApp : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(registerApp : (NSString *)appId : (RCTResponseSenderBlock)callback) {
   NSLog(@"%@", @"注册QQ");
-  self.tencentOAuth = [[TencentOAuth alloc] initWithAppId:[config objectForKey:@"appId"] andDelegate:self];
-}
-
-//判断是否安装了QQ
-RCT_EXPORT_METHOD(iphoneQQInstalled : (RCTResponseSenderBlock)callback) {
-
-  NSString *result = [NSString stringWithFormat:@"%d", [TencentOAuth iphoneQQInstalled]];
-
-  callback(@[result]);
-}
-//判断是否支持SSO授权登录QQ
-RCT_EXPORT_METHOD(iphoneQQSupportSSOLogin : (RCTResponseSenderBlock)callback) {
-
-  NSString *result = [NSString stringWithFormat:@"%d", [TencentOAuth iphoneQQSupportSSOLogin]];
-
-  callback(@[result]);
-}
-//判断是否安装了QQZone
-RCT_EXPORT_METHOD(iphoneQZoneInstalled : (RCTResponseSenderBlock)callback) {
-
-  NSString *result = [NSString stringWithFormat:@"%d", [TencentOAuth iphoneQZoneInstalled]];
-
-  callback(@[result]);
-}
-//判断是否支持SSO授权登录QQZone
-RCT_EXPORT_METHOD(iphoneQZoneSupportSSOLogin : (RCTResponseSenderBlock)callback) {
-
-  NSString *result = [NSString stringWithFormat:@"%d", [TencentOAuth iphoneQZoneSupportSSOLogin]];
-
-  callback(@[result]);
-}
-//用来获得当前sdk的版本号
-RCT_EXPORT_METHOD(sdkVersion : (RCTResponseSenderBlock)callback) {
-
-  NSString *result = [TencentOAuth sdkVersion];
-
-  callback(@[result]);
-}
-//用来获得当前sdk的小版本号
-RCT_EXPORT_METHOD(sdkSubVersion : (RCTResponseSenderBlock)callback) {
-
-  NSString *result = [TencentOAuth sdkSubVersion];
-
-  callback(@[result]);
-}
-//获取手机QQ版本号
-RCT_EXPORT_METHOD(iphoneQQVersion : (RCTResponseSenderBlock)callback) {
-
-  NSString *result = [NSString stringWithFormat:@"%d", [TencentOAuth iphoneQQVersion]];
-
-  callback(@[result]);
+  BOOL appRegistered = NO;
+  NSMutableDictionary *result = [NSMutableDictionary dictionaryWithCapacity:5];
+  if (appId && appId.length > 0) {
+    self.tencentOAuth = [[TencentOAuth alloc] initWithAppId:appId andDelegate:self];
+    [result setValue:[NSNumber numberWithBool:[TencentOAuth iphoneQQInstalled]] forKey: @"iphoneQQInstalled"];
+    [result setValue:[NSNumber numberWithBool:[TencentOAuth iphoneQZoneInstalled]] forKey: @"iphoneQZoneInstalled"];
+    [result setValue:[NSNumber numberWithBool:[TencentOAuth iphoneQQSupportSSOLogin]] forKey: @"iphoneQQSupportSSOLogin"];
+    [result setValue:[NSNumber numberWithBool:[TencentOAuth iphoneQZoneSupportSSOLogin]] forKey: @"iphoneQZoneSupportSSOLogin"];
+    callback(@[result]);
+  }
+  
 }
 
 - (BOOL)onTencentReq:(TencentApiReq *)req {
@@ -91,157 +51,153 @@ RCT_EXPORT_METHOD(iphoneQQVersion : (RCTResponseSenderBlock)callback) {
   NSLog(@"%@", @"接收resp");
   return YES;
 }
-//视频
-RCT_EXPORT_METHOD(sendVideo : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
 
-  NSLog(@"%@", @"qq视频分享");
+RCT_EXPORT_METHOD(share : (NSDictionary *)config : (RCTResponseSenderBlock)callback ) {
+  QQApiObject *object;
+  
+  NSString *title = [config objectForKey:@"title"];
+  NSString *description = [config objectForKey:@"description"];
+  NSURL *previewImageURL = [NSURL URLWithString:[config objectForKey:@"thumb"]];
 
-  QQApiVideoObject *videoObject =
-  [QQApiAudioObject objectWithURL:[NSURL URLWithString:[config objectForKey:@"url"]]
-                            title:[config objectForKey:@"title"]
-                      description:[config objectForKey:@"description"]
-                  previewImageURL:[NSURL URLWithString:[config objectForKey:@"previewImageURL"]]];
-
-  [videoObject setFlashURL:[NSURL URLWithString:[config objectForKey:@"flashURL"]]];
-
-  [self sendReqWithObject:videoObject config:config callback:callback];
+  NSURL *flashURL = [NSURL URLWithString:[config objectForKey:@"data"]];
+  
+  if ([config objectForKey:@"text"]) {
+    object = (QQApiTextObject *)object;
+    object = [QQApiTextObject objectWithText:[config objectForKey:@"text"]];
+  } else if ([config objectForKey:@"image"]) {
+    object = (QQApiImageObject *)object;
+    NSString *imageUrl = [config objectForKey:@"image"];
+    NSData *imageData;
+    if ([[imageUrl substringToIndex:1] isEqualToString:@"/"]) {
+      imageData = [NSData dataWithContentsOfFile:imageUrl];
+    } else {
+      imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]];
+    }
+    
+    object = [QQApiImageObject objectWithData:imageData
+                             previewImageData:imageData
+                                        title:title
+                                  description:description];
+  } else if([config objectForKey:@"webpage"]) {
+    object = (QQApiNewsObject *)object;
+    object = (QQApiNewsObject *)[QQApiNewsObject objectWithURL:[NSURL URLWithString:[config objectForKey:@"webpage"]]
+                                      title:title
+                                description:description
+                            previewImageURL:previewImageURL];
+  } else if([config objectForKey:@"music"]) {
+    object = (QQApiAudioObject *)object;
+    object =
+    (QQApiAudioObject *)[QQApiAudioObject objectWithURL:[NSURL URLWithString:[config objectForKey:@"music"]]
+                              title:title
+                        description:description
+                    previewImageURL:previewImageURL];
+    
+    [(QQApiAudioObject *)object setFlashURL:flashURL];
+    
+  } else if([config objectForKey:@"video"]) {
+    object = (QQApiVideoObject *)object;
+    object =
+    (QQApiVideoObject *)[QQApiVideoObject objectWithURL:[NSURL URLWithString:[config objectForKey:@"video"] ]
+                              title:title
+                        description:description
+                    previewImageURL:previewImageURL];
+    
+    [(QQApiVideoObject *)object setFlashURL:flashURL];
+  }
+  
+  NSString *scene = [config objectForKey:@"scene"];
+  
+  NSString *errorInfo;
+  
+  SendMessageToQQReq *req = [SendMessageToQQReq reqWithContent:object];
+  
+  if ([scene isEqualToString:@"qzone"]) {
+    errorInfo = [self getErrorInfoWithInt: [QQApiInterface SendReqToQZone:req]];
+  }  else {
+    errorInfo = [self getErrorInfoWithInt:[QQApiInterface sendReq:req]];
+  }
+  callback(@[errorInfo]);
 }
-//音频
-RCT_EXPORT_METHOD(sendMusic : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
 
-  NSLog(@"%@", @"qq音乐分享");
-
-  QQApiAudioObject *audioObject =
-  [QQApiAudioObject objectWithURL:[NSURL URLWithString:[config objectForKey:@"url"]]
-                            title:[config objectForKey:@"title"]
-                      description:[config objectForKey:@"description"]
-                  previewImageURL:[NSURL URLWithString:[config objectForKey:@"previewImageURL"]]];
-
-  [audioObject setFlashURL:[NSURL URLWithString:[config objectForKey:@"flashURL"]]];
-
-  [self sendReqWithObject:audioObject config:config callback:callback];
+- (NSString *)getErrorInfoWithInt:(QQApiSendResultCode)code {
+  NSString *errorInfo;
+  switch (code) {
+    case 0:
+      errorInfo = @"SENDSUCESS";
+      break;
+    case 1:
+      errorInfo = @"QQNOTINSTALLED";
+      break;
+    case 2:
+      errorInfo = @"QQNOTSUPPORTAPI";
+      break;
+    case 3:
+      errorInfo = @"MESSAGETYPEINVALID";
+      break;
+    case 4:
+      errorInfo = @"MESSAGECONTENTNULL";
+      break;
+    case 5:
+      errorInfo = @"MESSAGECONTENTINVALID";
+      break;
+    case 6:
+      errorInfo = @"APPNOTREGISTED";
+      break;
+    case 7:
+      errorInfo = @"APPSHAREASYNC";
+      break;
+    case 8:
+      errorInfo = @"QQNOTSUPPORTAPI_WITH_ERRORSHOW";
+      break;
+    case -1:
+      errorInfo = @"SENDFAILD";
+      break;
+    case 10000:
+      errorInfo = @"QZONENOTSUPPORTTEXT";
+      break;
+    case 10001:
+      errorInfo = @"QZONENOTSUPPORTIMAGE";
+      break;
+    default:
+      break;
+  }
+  
+  return errorInfo;
 }
-//网页链接
-RCT_EXPORT_METHOD(sendWebPage : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
 
-  NSLog(@"%@", @"qq网页分享");
-
-  QQApiNewsObject *newsObject = [QQApiNewsObject objectWithURL:[NSURL URLWithString:[config objectForKey:@"url"]]
-                                                         title:[config objectForKey:@"title"]
-                                                   description:[config objectForKey:@"description"]
-                                               previewImageURL:[NSURL URLWithString:[config objectForKey:@"previewImageURL"]]];
-
-  [self sendReqWithObject:newsObject config:config callback:callback];
-}
-//多图收藏
-RCT_EXPORT_METHOD(sendImageArray : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
-
-  NSLog(@"%@", @"qq多图分享");
-
-  NSArray *array = [config objectForKey:@"imageArray"];
-  NSMutableArray *images = [NSMutableArray arrayWithCapacity:array.count];
-  NSData *data = [NSData dataWithContentsOfFile:array[0]];
-
-  [array enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL *_Nonnull stop) {
-    NSData *data = [NSData dataWithContentsOfFile:obj];
-    [images addObject:data];
-  }];
-
-  QQApiImageObject *imageObject = [QQApiImageObject objectWithData:data
-                                                  previewImageData:[config objectForKey:@"previewImagePath"]
-                                                             title:[config objectForKey:@"title"]
-                                                       description:[config objectForKey:@"description"]
-                                                    imageDataArray:images];
-
-  [imageObject setCflag:kQQAPICtrlFlagQQShareFavorites];
-
-  [self sendReqWithObject:imageObject config:config callback:callback];
-}
-//图片
-RCT_EXPORT_METHOD(sendImage : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
-
-  NSLog(@"%@", @"qq图片分享");
-
-  NSData *data = [NSData dataWithContentsOfFile:[config objectForKey:@"imagePath"]];
-  NSData *previewImageData = [NSData dataWithContentsOfFile:[config objectForKey:@"previewImagePath"]];
-
-  QQApiImageObject *imageObject = [QQApiImageObject objectWithData:data
-                                                  previewImageData:previewImageData
-                                                             title:[config objectForKey:@"title"]
-                                                       description:[config objectForKey:@"description"]];
-
-  [self sendReqWithObject:imageObject config:config callback:callback];
-}
-//纯文本
-RCT_EXPORT_METHOD(sendText : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
-
-  NSLog(@"%@", @"qq文本分享");
-
-  QQApiTextObject *message = [QQApiTextObject objectWithText:[config objectForKey:@"text"]];
-
-  [self sendReqWithObject:message config:config callback:callback];
-}
 //授权登录
 RCT_EXPORT_METHOD(authorize : (NSDictionary *)config : (RCTResponseSenderBlock)callback) {
-
+  
   NSLog(@"申请QQ授权");
-
+  
   authCallback = callback;
-
+  
   NSMutableArray *permissions = [config objectForKey:@"permissions"];
   if (permissions.count == 0) {
     NSArray *defaultPermissions =
     [NSArray arrayWithObjects:kOPEN_PERMISSION_GET_USER_INFO, kOPEN_PERMISSION_GET_SIMPLE_USER_INFO, kOPEN_PERMISSION_ADD_SHARE, nil];
     [permissions addObjectsFromArray:defaultPermissions];
   }
-
   [self.tencentOAuth authorize:permissions];
 }
 
-- (void)sendReqWithObject:(QQApiObject *)object config:(NSDictionary *)config callback:(RCTResponseSenderBlock)callback {
-
-  NSString *scene = [config objectForKey:@"scene"];
-
-  NSMutableString *resultCode;
-
-  SendMessageToQQReq *req = [SendMessageToQQReq reqWithContent:object];
-
-  if ([scene isEqualToString:@"Qzone"]) {
-    resultCode = [NSMutableString stringWithFormat:@"%d", [QQApiInterface SendReqToQZone:req]];
-  }
-  if ([scene isEqualToString:@"QQGroup"]) {
-    resultCode = [NSMutableString stringWithFormat:@"%d", [QQApiInterface SendReqToQQGroupTribe:req]];
-  } else {
-    resultCode = [NSMutableString stringWithFormat:@"%d", [QQApiInterface sendReq:req]];
-  }
-
-  callback(@[resultCode]);
-}
-
 - (void)tencentDidLogin {
-
+  
   NSLog(@"授权登录");
-
+  
   [self.tencentOAuth getUserInfo];
-
+  
   if (self.tencentOAuth.accessToken && self.tencentOAuth.accessToken.length != 0) {
     NSLog(@"%@", self.tencentOAuth.accessToken);
     NSString *openId = self.tencentOAuth.openId;
     NSString *accessToken = self.tencentOAuth.accessToken;
     NSInteger expiresInSecond = [self.tencentOAuth.expirationDate timeIntervalSinceNow];
-
+    
     NSMutableDictionary *results = [[NSMutableDictionary alloc] initWithCapacity:4];
-
-    if (openId) {
-      [results setValue:openId forKey:@"openId"];
-    }
-    if (accessToken) {
-      [results setValue:accessToken forKey:@"accessToken"];
-    }
-    if (expiresInSecond > 0) {
-      [results setValue:[NSNumber numberWithInteger:expiresInSecond] forKey:@"expiresInSecond"];
-    }
-
+    [results setValue:openId forKey:@"openId"];
+    [results setValue:accessToken forKey:@"accessToken"];
+    [results setValue:[NSNumber numberWithInteger:expiresInSecond] forKey:@"expiresInSecond"];
+    
     authCallback(@[results]);
     authCallback = nil;
   }
@@ -249,28 +205,28 @@ RCT_EXPORT_METHOD(authorize : (NSDictionary *)config : (RCTResponseSenderBlock)c
 
 - (void)getUserInfoResponse:(APIResponse *)response {
   NSLog(@"获取用户信息");
-
+  
   NSLog(@"%@", response.jsonResponse);
 }
 
 - (void)tencentDidNotLogin:(BOOL)cancelled {
-
+  
   NSMutableDictionary *error = [[NSMutableDictionary alloc] init];
   if (cancelled) {
-    [error setValue:[NSNumber numberWithBool:YES] forKey:@"cancel"];
+    [error setValue:[NSNumber numberWithBool:YES] forKey:@"uesr cancel"];
   } else {
     [error setValue:@"不知名原因导致登陆失败" forKey:@"error"];
   }
-
+  
   authCallback(@[error]);
   authCallback = nil;
 }
 
 - (void)tencentDidNotNetWork {
-
+  
   NSMutableDictionary *error = [[NSMutableDictionary alloc] init];
   [error setValue:@"当前网络不可用,请设置网络" forKey:@"error"];
-
+  
   authCallback(@[error]);
   authCallback = nil;
 }
